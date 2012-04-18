d4ee3acd968bbed5fa31319470bcb307ba7c3178
Matthew DiBernardo <matthew.dibernardo@escapemg.com>
2012-04-17 20:45:49 -0400

    Lots of updates and additions to the rtp libs. Also added an rtp.pkg file, for use with the new apt-get program I wrote on the side. apt-get should now be able to auto-fetch all the files in the rtp package via wget.

---
+++ titan/dns_host.lua
@@ -0,0 +1,16 @@
+--[[
+    dns-host.lua
+    
+    A DNS host for RTP. Used for resolving RP Addresses (equivalent to real-world
+    IP Addresses)
+    
+    Matthew DiBernardo [04.11.2012]
+]]--
+
+local cacheDir = "/var/cache/dns"
+
+DnsHost = {}
+
+function DnsHost:listen()
+    
+end
--- titan/titan.lua
+++ titan/titan.lua
@@ -1,11 +1,51 @@
 --[[
-    titan.lua
+    titan
     
-    Apache equivalent.
+    An RTP webhost. Titan is basically analogous to real-world Apache.
     
-    Matthew DiBernardo [04.14.2012]
+    Matthew DiBeranrdo [04.13.2012]
 ]]--
 
--- Includes
+-- Main Titan Class
+Titan = {}
 
--- Constants
+function Titan:start()
+
+end
+
+
+-- Titan Listen Server
+function rtpListen()
+    while true do
+        msg = rtp.listen(10) -- Wait for a message
+        if msg ~= nil then
+            mArr = json.Decode(msg)
+            os.pushEvent("RTP-"..mArr["Type"], mArr)
+        end
+    end
+end
+
+
+-- Titan Event Handler
+EventHandler = {_e = {}}
+
+function EventHandler:registerEvent(event, method)
+    self._e[event] = method
+end
+
+function EventHandler:unregisterEvent(event)
+    self._e[event] = nil
+end
+
+function EventHandler:handle(event, msg)
+    if self._e[event] != nil then
+        return self._e[event](msg)
+    end
+end
+
+function EventHandler:listen()
+    while true do
+        event, msg = os.pullEventRaw()
+        status = EventHandler:handle(event, msg)
+    end
+end
